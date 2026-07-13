5d4b3aea001277034855df8d84a34f084013472c
num <119519462+utsav9904@users.noreply.github.com>
2026-07-13 15:23:00 +0530

    fix: replace html2canvas with html2canvas-pro to support oklch colors in PDF generation

--- client/src/utils/pdfExport.ts
+++ client/src/utils/pdfExport.ts
@@ -1,4 +1,4 @@
-import html2canvas from 'html2canvas';
+import html2canvas from 'html2canvas-pro';
 import jsPDF from 'jspdf';
 
 export const generatePDF = async (elementId: string, filename: string = 'resume.pdf') => {
